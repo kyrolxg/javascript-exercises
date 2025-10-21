bac390cb148394660dc185d4ef66ebf642c577b2
kyro <71367144+kyrolxg@users.noreply.github.com>
2025-10-21 17:38:37 +0530

    Successfully completed exercises 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num<0) return "ERROR";
+    let copyStr = '';
+    for(let i = 0; i<num; i++){
+        copyStr+=str;
+    }
+    return copyStr;
 };
 
 // Do not edit below this line
