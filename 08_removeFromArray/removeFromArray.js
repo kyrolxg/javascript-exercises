482000c4a7eb6275f45e783ee4fc60ad0a432096
kyro <71367144+kyrolxg@users.noreply.github.com>
2025-10-24 22:44:53 +0530

    Completed the 8th exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    let result = [];
+    for(const val of arr){
+        if (!values.includes(val)){
+            result.push(val);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
