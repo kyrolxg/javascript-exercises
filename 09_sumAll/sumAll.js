f719f76a899b62ace0cd8712da013c4fb6710f2e
kyro <71367144+kyrolxg@users.noreply.github.com>
2025-10-24 23:30:33 +0530

    Complete the 9th exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0
+        || !Number.isFinite(a) || !Number.isFinite(b)
+        || !Number.isInteger(a) || !Number.isInteger(b)
+    ) return "ERROR";
 
+    let sum = 0;
+    let larger, smaller;
+    if(a>b){
+        larger = a;
+        smaller = b;
+    } else{
+        larger = b;
+        smaller = a;
+    }
+    for(let i = smaller; i<=larger; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
