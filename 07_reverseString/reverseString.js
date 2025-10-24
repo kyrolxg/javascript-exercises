1b0aceec0f688098b9141d8f5e4a6b336ebe9802
kyro <71367144+kyrolxg@users.noreply.github.com>
2025-10-24 18:58:37 +0530

    Completed the 7th exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArr = str.split("");
+    strArr.reverse();
+    str = strArr.join("");
+    return str;
 };
 
 // Do not edit below this line
