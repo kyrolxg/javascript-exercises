0ffabf69981176b434ab9144c36356bccf62b0dd
kyro <71367144+kyrolxg@users.noreply.github.com>
2025-10-25 18:46:19 +0530

    Completed the 11th exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let result = (degrees - 32) * (5/9);
+  if(Number.isInteger(result)) return result;
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let result = degrees*(9/5) + 32;
+  if(Number.isInteger(result)) return result;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
